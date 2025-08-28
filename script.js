26d116bf9f0694b15fce1c2d2ef92e3b57534188
SRManager3214 <srmgr@jgrls.org>
2025-08-28 09:04:26 -0500

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  document.getElementById("year").textContent = new Date().getFullYear();
-  document.getElementById("searchBtn").addEventListener("click", () => {
-    alert("Search functionality is not implemented in this demo.");
-  });
-});
